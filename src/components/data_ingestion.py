343d04d484048ed042ec9dc3b7cc13a3fc1a49f3
Sam-DataWizard <sampathvuthur2022.com>
2024-07-11 18:11:44 -0500

    Data Ingestion for CVS tables

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -20,8 +20,30 @@ class DataIngestion:
     def initiate_data_ingestion(self):
         logging.info("Entered the data ingestion method or component")
         try:
-            df=pd.read_csv('')
+            df=pd.read_csv('student-dataset.csv')
             logging.info('Read the dataframe')
 
-            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=)
+            os.makedirs(os.path.dirname(self.ingestion_config.train_data_path),exist_ok=True)
 
+            df.to_csv(self.ingestion_config.raw_data_path,index=False,header=True)
+
+            logging.info("Train test split initiated")
+            train_set,test_set=train_test_split(df,test_size=0.2,random_state=42)
+
+            train_set.to_csv(self.ingestion_config.train_data_path,index=False,header=True)
+
+            test_set.to_csv(self.ingestion_config.test_data_path,index=False,header=True)
+
+            logging.info("Ingestion of the data is completed")
+
+            return(
+                self.ingestion_config.train_data_path,
+                self.ingestion_config.test_data_path
+            )
+        except Exception as e:
+            raise CustomException(e,sys)
+        
+if __name__== "__main__":
+    obj=DataIngestion()
+    obj.initiate_data_ingestion()
+    
